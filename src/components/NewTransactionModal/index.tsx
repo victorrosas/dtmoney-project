196d671a0e3f0f6e2ba9365ad4a14dcd91fbb8de
Victor Rosas <victor.rosas.96@hotmail.com>
2021-03-31 23:15:50 -0500

    button type modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -35,6 +37,18 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
                 placeholder="Valor"
                 type="number"
             />
+
+            <TransactionTypeContainer>
+                <button type="button">
+                    <img src={incomeImg} alt="Entrada"/>
+                    <span>Entrada</span>
+                </button>
+
+                <button type="button">
+                    <img src={outcomeImg} alt="Saída"/>
+                    <span>Entrada</span>
+                </button>
+            </TransactionTypeContainer>
 
             <input 
                 placeholder="Catgoria"
