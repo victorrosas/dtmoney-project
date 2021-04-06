33f7e088ea54e9f24a46ec706e18034ee063fd9d
Victor Rosas <victor.rosas.96@hotmail.com>
2021-04-05 22:13:55 -0500

    context api and provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from "./styles/global";
+import { TransactionsProvider } from './TransactionsContext';
 
 Modal.setAppElement('#root');
 
@@ -19,7 +20,7 @@ export function App() {
       setIsNewTransactionModalOpen(false);
   }
   return (
-    <>
+    <TransactionsProvider>
      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/> 
 
      <Dashboard />
@@ -30,6 +31,6 @@ export function App() {
      />
 
      <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
